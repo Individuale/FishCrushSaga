c302b5e7653b30f8cb3be5019af2c8757efb4895
Faycel2015 <faycel.ayech@gmail.com>
2021-11-24 22:38:07 +0100

    Imported Unity Project

---
+++ FishCrushSaga/Assets/Scripts/ClearLinePiece.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearLinePiece : ClearablePiece
+{
+
+    public bool isRow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Clear()
+    {
+        base.Clear();
+
+        if (isRow)
+        {
+            // Clear row
+            piece.GridRef.ClearRow(piece.Y);
+
+        }
+        else
+        {
+            // Clear column
+            piece.GridRef.ClearColumn(piece.X);
+
+        }
+    }
+}
